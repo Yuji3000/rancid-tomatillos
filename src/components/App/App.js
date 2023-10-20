260bfe1ddb5aacbde9cc6ff5738abc2250e709d7
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-10-20 15:12:42 -0600

    revert changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-// import { movieData } from '../../movieApi/movieData.js';
+import { movieData } from '../../movieApi/movieData.js';
 import MovieCards from '../MovieCards/MovieCards';
 import Header from '../Header/Header';
-import { useState, useEffect} from 'react'
-import fetch from 'node-fetch';
+import { useState } from 'react'
 
 function App() {
-  const [movieCards, setMovies] = useState([])
-  
-  useEffect(() => {
-    getAllMovies();
-  }, []); 
+  const [movieCards] = useState(movieData)
 
-  function getAllMovies(){
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => response.json())
-    .then(data => setMovies(data))
-    .catch(error => console.log(error.message))
-  }
- 
   return (
     <main className="App">
       <Header />
--- src/components/MovieCards/MovieCards.js
+++ src/components/MovieCards/MovieCards.js
@@ -1,13 +1,13 @@
 import "./MovieCards.css";
 
 
-function MovieCards({ allMovieData }) {
+function MovieCards({ allMovieData}) {
   return allMovieData.movies.map((movie) => {
     return (
-        <div className="thumbnail">
-          <img src={movie.poster_path} className="movie-img" alt="oppsie"/>
-          <h2 className="movie-titles">{`${movie.title}`}</h2>
-        </div>
+      <div className="thumbnail">
+        <img src={movie.poster_path} className="movie-img" alt="oppsie"/>
+        <h2 className="movie-titles">{`${movie.title}`}</h2>
+      </div>
     );
   });
 }
