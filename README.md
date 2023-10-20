8eef1278a7f9c238c9c7c4b7be3e6c2a5779a14d
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-10-20 12:38:32 -0600

    Now fetching all movie data from api instead of hardcoded data

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
+# Rotten Tomatillos
+
+Learning Objectives
+- Practice React.js fundatmentals with this use case
+- Create a UI with data from an external API
+  - GET, POST, DELETE requests
+- Create a multi-page UX using Router
+
+
 # Iteration 1
-- Display All Movies from static Data
+ Display All Movies from static Data
 
 <img src="src/assets/readme/iteration1.png" class="center" width="600" height="auto">
+
+# Iteration 2
+
+# Iteration 3
+
+# Iteration 4
+
+# Iteration 5
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import { movieData } from '../../movieApi/movieData.js';
+// import { movieData } from '../../movieApi/movieData.js';
 import MovieCards from '../MovieCards/MovieCards';
 import Header from '../Header/Header';
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import fetch from 'node-fetch';
 
 function App() {
-  const [movieCards] = useState(movieData)
+  const [movieCards, setMovies] = useState([])
+  
+  useEffect(() => {
+    getAllMovies();
+  }, []); 
 
+  function getAllMovies(){
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+    .then(data => setMovies(data))
+    .catch(error => console.log(error.message))
+  }
+ 
   return (
     <main className="App">
       <Header />
