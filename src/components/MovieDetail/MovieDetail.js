b166ba2a2d7a02bececdbc4882433e3fb5b2e824
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-11-07 15:16:56 -0700

    Add youtube videos to moviedetails pages

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,57 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import "./MovieDetail.css"
-import { getAllData } from '../../api-calls'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react';
+import './MovieDetail.css';
+import { getAllData } from '../../api-calls';
+import PropTypes from 'prop-types';
 
 function MovieDetail({ movieId }) {
-  const [movie, setMovie] = useState(null)
+  const [movie, setMovie] = useState(null);
+  const [trailer, setTrailer] = useState(null);
 
   useEffect(() => {
-    getAllData(`/movies/${movieId}`)
-      .then(data => {
-        setMovie(data[0].movie)
-      })
-      .catch(error => {
-        console.error('Error fetching movie details')
-      })
-  }, [movieId])
+    const fetchData = async () => {
+      try {
+        const videoData = await getAllData(`/movies/${movieId}/videos`);
+        const foundTrailer = findTrailer(videoData[0].videos);
+        setTrailer(foundTrailer);
+      } catch (error) {
+        console.error('Error fetching video data:', error);
+      }
+    };
+
+    fetchData();
+  }, [movieId]);
+
+  function findTrailer(videos) {
+    for (let i = 0; i < videos.length; i++) {
+      if (videos[i].type === 'Trailer') {
+        return videos[i].key;
+      }
+    }
+    return videos[0].key;
+  }
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const movieData = await getAllData(`/movies/${movieId}`);
+        setMovie(movieData[0].movie);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+
+    fetchMovieData();
+  }, [movieId]);
 
   if (!movie) {
-    return <p className='loading'>Loading...</p>
-  } else {
-    console.log(movie);
-    return (
-      <section>
-        <div>
-          <div className="image-container">
-            <img src={movie.backdrop_path} className="movie-backdrop" alt={movie.title}></img>
-          </div>
-          <section className="movie-details-container">
-            <div className="movie-details-box">
-              <h2 className="mTitle">{movie.title}{movie.tag_line}</h2>
-              <p className="single-movie-overview">{movie.overview}</p>
-              <p className="single-movie-release-date">{movie.release_date}</p>
-              <p className="single-movie-release-date">{movie.genres.join(', ')}</p>
-            </div>
-          </section>
-        </div>
-      </section>
-    )
+    return <p className="loading">Loading...</p>;
   }
+
+  return (
+    <section>
+      <div>
+        <div className="image-container">
+          <img src={movie.backdrop_path} className="movie-backdrop" alt={movie.title} />
+        </div>
+        <section className="movie-details-container">
+          <div className="movie-details-box">
+            <iframe
+              title="YouTube movie trailer"
+              className="selected-movie-trailer"
+              width="92%"
+              height="400px"
+              src={`https://www.youtube.com/embed/${trailer}`}
+            ></iframe>
+            <h2 className="mTitle">{movie.title}{movie.tag_line}</h2>
+            <p className="single-movie-overview">{movie.overview}</p>
+            <p className="single-movie-release-date">{movie.release_date}</p>
+            <p className="single-movie-release-date">{movie.genres.join(', ')}</p>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
 }
 
-export default MovieDetail
-
 MovieDetail.propTypes = {
-  moviedetail: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      backdrop_path: PropTypes.string.isRequired,
-      tag_line: PropTypes.string.isRequired,
-      overview: PropTypes.string.isRequired,
-      release_date: PropTypes.string.isRequired,
-      genres: PropTypes.array.isRequired,
-    })
-  )
+  movieId: PropTypes.number.isRequired,
 };
+
+export default MovieDetail;
