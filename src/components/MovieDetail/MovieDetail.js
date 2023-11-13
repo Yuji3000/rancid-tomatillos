58a8c092e00d02f390012f91cdd44e1d24ea6499
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-11-13 14:56:44 -0700

    Add basic formatting to a movie detail page

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -50,23 +50,27 @@ function MovieDetail({ movieId }) {
   return (
     <section>
       <div>
+        
+        <div className='trailer-container'>
+          <iframe
+            title="YouTube movie trailer"
+            className="selected-movie-trailer"
+            width="92%"
+            height="400px"
+            src={`https://www.youtube.com/embed/${trailer}`}
+          ></iframe>
+        </div>
         <div className="image-container">
           <img src={movie.backdrop_path} className="movie-backdrop" alt={movie.title} />
-        </div>
-        <section className="movie-details-container">
           <div className="movie-details-box">
-            <iframe
-              title="YouTube movie trailer"
-              className="selected-movie-trailer"
-              width="92%"
-              height="400px"
-              src={`https://www.youtube.com/embed/${trailer}`}
-            ></iframe>
             <h2 className="mTitle">{movie.title}{movie.tag_line}</h2>
             <p className="single-movie-overview">{movie.overview}</p>
             <p className="single-movie-release-date">{movie.release_date}</p>
             <p className="single-movie-release-date">{movie.genres.join(', ')}</p>
           </div>
+        </div>
+
+        <section className="movie-details-container">
         </section>
       </div>
     </section>
